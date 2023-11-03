2e6a4ab310f8ce51be377285056f775ce2c0fbf9
ChemInCode <140384545+ChemInCode@users.noreply.github.com>
2023-11-03 17:20:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
-import streamlit as st
+"""
+
+hello
+"""
 
 try:
+    import streamlit as st
     st.write('''<iframe src="http：//www.baidu.com" width="800px" height="600px"></iframe>''', unsafe_allow_html=True)
 except Exception as e:
     st.write(e)
