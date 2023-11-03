8273fddf1c4666b0a3e19b7cd4979b197b0f47db
ChemInCode <140384545+ChemInCode@users.noreply.github.com>
2023-11-03 16:21:59 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
-import CoolProp.CoolProp
+import CoolProp.CoolProp as CP
 
 st.write(CP.PropsSI('D', 'P', 101325, 'T', 25+273.15, 'Air'))
 """
