8275c8f8e33f8e69d83d4aeea6f6abd0a55b20d4
ChemInCode <140384545+ChemInCode@users.noreply.github.com>
2023-11-03 16:26:41 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
-import CoolProp.CoolProp
+import CoolProp
 
 """
 # Welcome to Streamlit!
@@ -14,7 +14,7 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-num_points = st.slider("Your Number of points in spiral"+f"{CoolProp.CoolProp.PropsSI('D', 'P', 101325, 'T', 25+273.15, 'Air')}", 1, 10000, 1100)
+num_points = st.slider("Your Number of points in spiral"+f"{CoolProp.CoolProp.PropsSI('D', 'P', 101325, 'T', 25+273.15, 'Air')} ---", 1, 10000, 1100)
 num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
 
 indices = np.linspace(0, 1, num_points)
